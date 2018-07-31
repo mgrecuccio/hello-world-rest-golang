cd61889c65ea16725f6a7f6f9ebd8ed0c78cafb0
Prakash Pandey <pcpandey@mail.com>
2018-08-01 03:10:06 +0530

    modifed for google app engine

--- main.go
+++ main.go
@@ -2,36 +2,82 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"google.golang.org/appengine" // Required external App Engine library
-	"os"
 )
 
-func determineListnerAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// if statement redirects all invalid URLs to the root homepage.
+	// Ex: if URL is http://[YOUR_PROJECT_ID].appspot.com/FOO, it will be
+	// redirected to http://[YOUR_PROJECT_ID].appspot.com.
+	if r.URL.Path != "/" {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
 	}
-	return ":" + port, nil
-}
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, from Tenx Infotech")
+	fmt.Fprintln(w, "Hello, From Tenx Infotech!")
 }
 
 func main() {
-	addr, err := determineListnerAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
-	http.HandleFunc("/", hello)
-	log.Printf("Listening os %s...\n", addr)
-
-	// Starting listening
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err)
-	}
-	// Starts the server to receive requests
-	appengine.Main()
+	http.HandleFunc("/", indexHandler)
+	appengine.Main() // Starts the server to receive requests
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+
+// 	"google.golang.org/appengine" // Required external App Engine library
+// )
+
+// func determineListnerAddress() (string, error) {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		return "", fmt.Errorf("$PORT not set")
+// 	}
+// 	return ":" + port, nil
+// }
+
+// func hello(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "Hello, from Tenx Infotech")
+// }
+
+// func main() {
+// 	addr, err := determineListnerAddress()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	http.HandleFunc("/", hello)
+// 	log.Printf("Listening os %s...\n", addr)
+
+// 	// Starting listening
+// 	if err := http.ListenAndServe(addr, nil); err != nil {
+// 		panic(err)
+// 	}
+// 	// Starts the server to receive requests
+// 	appengine.Main()
+// }
