6f3d74f2f37b6d834326c7ba7b747fc808bb63bd
mgrecuccio <marco.grecuccio@outlook.com>
2024-03-14 12:11:39 +0100

    Creating hello-world endpoint

--- main.go
+++ main.go
@@ -3,45 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"google.golang.org/appengine" // Required external App Engine library
+	"os" // Required external App Engine library
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	// if statement redirects all invalid URLs to the root homepage.
-	// Ex: if URL is http://[YOUR_PROJECT_ID].appspot.com/FOO, it will be
-	// redirected to http://[YOUR_PROJECT_ID].appspot.com.
-	if r.URL.Path != "/" {
-			http.Redirect(w, r, "/", http.StatusFound)
-			return
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := os.Getenv("RESPONSE")
+	if len(response) == 0 {
+		response = "Hello OpenShift for Developers!"
 	}
-
-	fmt.Fprintln(w, "Hello, From Tenx Infotech!")
+	fmt.Fprintln(w, response)
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	appengine.Main() // Starts the server to receive requests
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // package main
 
